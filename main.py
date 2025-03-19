a780a686117b89652c415dfd1ff9998bb3d79df4
ev_b <rainbowviola3@gmail.com>
2025-03-19 19:42:27 +0300

    Created project

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def login():
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def reqister(): 
+def reqister():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
